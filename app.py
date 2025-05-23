fbde6ff52670c12a5b89317054370a17b71c3ef3
sriram <159551306+sriram-65@users.noreply.github.com>
2025-05-23 14:18:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask , redirect , request , session , url_for , render_template 
+from pymongo import MongoClient
+import cloudinary
+import cloudinary.uploader
+
+app = Flask(__name__)
+
+client = MongoClient("mongodb+srv://sriram65raja:1324sriram@cluster0.dejys.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+data_base = client["ai"]
+TEMPLATES_DB = data_base["TEMPLATES_DB"]
+
+cloudinary.config(
+     cloud_name="dbrmvywb0",
+    api_key="799647841433247",
+    api_secret="XLtCOYXxRTnjZqwaF2oFnQ0AK7k"
+)
+
+
+@app.route("/")
+def home():
+    get_templates = TEMPLATES_DB.find({}).sort("_id" , -1)
+    return render_template("index.html" , templates = get_templates)
+
+
+
+@app.route("/upload-Template" , methods=["POST" , "GET"])
+def upload():
+    if(request.method=="POST"):
+        title = request.form.get("title")
+        category = request.form.get("category")
+        img = request.files.get("img")
+        des = request.files.get("des")
+        live_previwe_link = request.form.get("link")
+        
+        if(not title and not category and not img and live_previwe_link):
+            return "Unable to Upload"
+        
+        upload_to_cloud = cloudinary.uploader.upload(img , resource_type="auto")
+        img_url = upload_to_cloud["secure_url"]
+        data={
+            "title":title,
+            "category":category,
+            "img":img_url,
+            "descption":des,
+             "Links":live_previwe_link
+        }
+        
+        TEMPLATES_DB.insert_one(data)
+        return redirect("/")
+    return render_template("admin.html")
+        
+
+if __name__ == "__main__":
+    app.run(debug=True)
