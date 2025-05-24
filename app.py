e26b47f16c44fcb036596ece0c3272c1629aa563
sriram <159551306+sriram-65@users.noreply.github.com>
2025-05-24 16:05:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 client = MongoClient("mongodb+srv://sriram65raja:1324sriram@cluster0.dejys.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 data_base = client["ai"]
 TEMPLATES_DB = data_base["TEMPLATES_DB"]
+COUSTOMERS_DB = data_base["COUSTOMERS_DB"]
 
 cloudinary.config(
      cloud_name="dbrmvywb0",
@@ -49,7 +50,8 @@ def upload():
         TEMPLATES_DB.insert_one(data)
         return redirect("/")
     get_templates = TEMPLATES_DB.find({}).sort("_id" , -1)
-    return render_template("admin.html" , templates = get_templates)
+    get_cousterms_details_info = COUSTOMERS_DB.find({}).sort("_id" , -1)
+    return render_template("admin.html" , templates = get_templates , deatils = get_cousterms_details_info )
         
         
 
@@ -60,7 +62,50 @@ def delete_post(template_id):
         return redirect("/")
     except:
         return "Server : Unable handle Your request"
-        
+    
+
+@app.route("/upload-information-about-shops" , methods=["POST"])
+def shops():
+    shop_owner_name = request.form.get("soname")
+    phone_no = request.form.get("phoneno")
+    shop_adress = request.form.get("address")
+    shop_name = request.form.get("shop_name")
+    business_type = request.form.get("business_type")
+    shop_logo = request.form.get('logo')
+    description = request.form.get('description')
+    products = request.form.get('products')
+    website_type= request.form.get("website_type")
+    advance_payment = request.form.get("advance")
+    img_upload = request.files.getlist("img-upload")
+    payment_mode = request.form.get("payment_mode")
+    
+    
+    upload_img_url = []
+    
+    for files in img_upload:
+        result = cloudinary.uploader.upload(files , resource_type="auto" )
+        upload_img_url.append(result["secure_url"])
+    
+    data = {
+        "shop_owner_name":shop_owner_name,
+        "phone_no":phone_no,
+        "shop_adress":shop_adress,
+        "shop_name":shop_name,
+        "business_type":business_type, 
+        "shop_logo":shop_logo,
+        "description":description,
+        "products":products,
+        "website_type":website_type,
+        "advance_payment":advance_payment,
+        "payment_mode":payment_mode,
+        "imgs":upload_img_url
+    }
+    
+    COUSTOMERS_DB.insert_one(data)
+    return redirect("/")
+    
+
+
     
 if __name__ == "__main__":
     app.run(debug=True)
